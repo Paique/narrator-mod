294e892e25ced9ccbef1b71687d6d62e9f07daba
paique <bielperrett20@gmail.com>
2024-03-27 14:06:17 -0300

    forge (message) fix.

--- src/main/java/net/paiique/brpacks/narrator/forge/util/BulkPlayerMessage.java
+++ src/main/java/net/paiique/brpacks/narrator/forge/util/BulkPlayerMessage.java
@@ -1,22 +1,22 @@
 package net.paiique.brpacks.narrator.forge.util;
 
-import io.netty.util.internal.UnstableApi;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.server.ServerLifecycleHooks;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BulkPlayerMessage {
-    public static void sendSystemMessage(String message, boolean actionBar) {
+    public static void sendMessage(String message, boolean actionBar) {
         if (ServerLifecycleHooks.getCurrentServer() == null) return;
         ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers().forEach(serverPlayer -> {
             serverPlayer.sendSystemMessage(Component.literal(message), actionBar);
         });
     }
 
-    public static void sendMessage(List<String> messages) {
+    public static void sendMessage(LinkedList<String> messages) {
         for (String message : messages) {
-            sendSystemMessage(message, false);
+            sendMessage(message, false);
         }
     }
 
