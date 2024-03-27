cd9702f60a70899b09f8bce367d2d233182aa021
paique <bielperrett20@gmail.com>
2024-03-27 14:09:01 -0300

    API (feat): Added a request builder.

---
+++ src/main/java/net/paiique/brpacks/narrator/exeptions/HttpsRequest/InvalidRequestFieldExeption.java
@@ -0,0 +1,9 @@
+package net.paiique.brpacks.narrator.exeptions.HttpsRequest;
+
+public class InvalidRequestFieldExeption extends Exception {
+    public InvalidRequestFieldExeption() {}
+
+    public InvalidRequestFieldExeption(String message) {
+        super(message);
+    }
+}
