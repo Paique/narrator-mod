7a760cbda7f371505e5c242d59d5429c699b438a
paique <bielperrett20@gmail.com>
2024-03-27 14:06:30 -0300

    forge (config): debug added.

--- src/main/java/net/paiique/brpacks/narrator/forge/config/ConfigCommon.java
+++ src/main/java/net/paiique/brpacks/narrator/forge/config/ConfigCommon.java
@@ -12,6 +12,8 @@ public class ConfigCommon {
     public static final ForgeConfigSpec.ConfigValue<Integer> NARRATOR_VOLUME;
     public static final ForgeConfigSpec.ConfigValue<Integer> REQUIRED_ACTIONS_POINTS;
     public static final ForgeConfigSpec.ConfigValue<Integer> CHECK_COOLDOWN;
+    public static final ForgeConfigSpec.ConfigValue<Float> NARRATOR_TEMPERATURE;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> DEBUG;
 
 
     static {
@@ -19,6 +21,11 @@ public class ConfigCommon {
 
         OPENAI_API_KEY = BUILDER.comment("OpenAI API Key (String)")
                 .define("openai_api_key", "CHANGE_ME");
+
+        NARRATOR_VOLUME = BUILDER.comment("Narrator Volume Percentage (Int)")
+                .define("narrator_volume", 100);
+
+        BUILDER.comment("[Advanced] (Do not change unless you know what you are doing!)");
 
         CHAT_GPT_MODEL = BUILDER.comment("Chat GPT Model (String)")
                 .define("chat_gpt_model", "gpt-3.5-turbo");
@@ -29,14 +36,19 @@ public class ConfigCommon {
         VOICE = BUILDER.comment("Voice (String)")
                 .define("voice", "onyx");
 
-        NARRATOR_VOLUME = BUILDER.comment("Narrator Volume Percentage (Int)")
-                .define("narrator_volume", 100);
-
         REQUIRED_ACTIONS_POINTS = BUILDER.comment("Required Action Points (Integer)")
                 .define("required_action_points", 300);
 
+        NARRATOR_TEMPERATURE = BUILDER.comment("Narrator Temperature (Float)")
+                .define("narrator_temperature", 0.3f);
+
         CHECK_COOLDOWN = BUILDER.comment("Cooldown between checks (Integer)")
                 .define("check_every_x_ticks", 200);
+
+        BUILDER.comment("Developer settings");
+
+        DEBUG = BUILDER.comment("Debug mode (boolean)")
+                        .define("debug", false);
 
         BUILDER.pop();
         COMMON_SPEC = BUILDER.build();
