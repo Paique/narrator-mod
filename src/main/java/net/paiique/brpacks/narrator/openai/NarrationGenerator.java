8578260f218a04e1f28a2065418eff0e1138ec89
paique <bielperrett20@gmail.com>
2024-03-27 14:10:37 -0300

    Fix (debug): Removed debug text

--- src/main/java/net/paiique/brpacks/narrator/openai/NarrationGenerator.java
+++ src/main/java/net/paiique/brpacks/narrator/openai/NarrationGenerator.java
@@ -1,15 +1,19 @@
 package net.paiique.brpacks.narrator.openai;
 
+import com.mojang.logging.LogUtils;
 import net.paiique.brpacks.narrator.NarratorMod;
 import net.paiique.brpacks.narrator.data.Data;
 import net.paiique.brpacks.narrator.forge.config.ConfigCommon;
 import net.paiique.brpacks.narrator.forge.network.PacketHandler;
 import net.paiique.brpacks.narrator.forge.network.SCPlaySoundPacket;
 import net.paiique.brpacks.narrator.util.FFmpeg;
+import org.slf4j.Logger;
 
 import java.nio.file.Path;
 
 public class NarrationGenerator extends Data {
+
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     private CompletationPost completation;
     private VoicePost voicePost;
@@ -23,6 +27,8 @@ public class NarrationGenerator extends Data {
         if (completation == null) completation = new CompletationPost();
         String generatedText = completation.generate(openAiToken, actualAiText);
         if (generatedText.isEmpty()) return;
+        LOGGER.info("Generated text: " + generatedText);
+
 
         if (voicePost == null) voicePost = new VoicePost();
         Path narratorFile = voicePost.generate(openAiToken, generatedText);
