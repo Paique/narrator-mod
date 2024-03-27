132b0fea2fe4a962c4eba228277bb9c7a4f0ed2a
paique <bielperrett20@gmail.com>
2024-03-27 14:11:31 -0300

    exception (feat): Added a new unused (for now) exception.

---
+++ src/main/java/net/paiique/brpacks/narrator/exeptions/HttpsRequest/RequestFaliedException.java
@@ -0,0 +1,9 @@
+package net.paiique.brpacks.narrator.exeptions.HttpsRequest;
+
+public class RequestFaliedException extends Exception {
+    public RequestFaliedException() {}
+
+    public RequestFaliedException(String message) {
+        super(message);
+    }
+}
